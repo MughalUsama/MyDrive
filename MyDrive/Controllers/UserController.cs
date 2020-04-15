e7bc55c58cf5a3d64dec3855d125c3a47c4e9fd1
Usama Shahid <mughal.usama1@yahoo.com>
2020-04-15 17:47:02 +0500

    Created Views

---
+++ MyDrive/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyDrive.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            return Redirect("~/Home/Index");
+        }
+    }
+}
